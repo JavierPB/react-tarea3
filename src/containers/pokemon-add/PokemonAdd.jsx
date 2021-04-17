0361961a9d9e236ad0e9ba32981b5fdc91fc66b0
Javier Pérez Barrientos <javier.jperez@gmail.com>
2021-04-17 08:27:29 -0400

    ajuste visual

--- src/containers/pokemon-add/PokemonAdd.jsx
+++ src/containers/pokemon-add/PokemonAdd.jsx
@@ -42,12 +42,18 @@ const PokemonAdd = () => {
       <div className="card-body">
         <div className="form">
           <FormItem name="name" label="Nombre" handleChange={handleChange} value={formData.name} />
-          <FormItem name="HP" type="number" label="HP" handleChange={handleChange} value={formData.HP} />
-          <FormItem name="Attack" type="number" label="Attack" handleChange={handleChange} value={formData.Attack} />
-          <FormItem name="Defense" type="number" label="Defense" handleChange={handleChange} value={formData.Defense} />
-          <FormItem name="SpAttack" type="number" label="Sp. Attack" handleChange={handleChange} value={formData.SpAttack} />
-          <FormItem name="SpDefense" type="number" label="Sp. Defense" handleChange={handleChange} value={formData.SpDefense} />
-          <FormItem name="Speed" type="number" label="Speed" handleChange={handleChange} value={formData.Speed} />
+          <div className="row">
+            <div className="col-lg-6">
+              <FormItem name="HP" type="number" label="HP" handleChange={handleChange} value={formData.HP} />
+              <FormItem name="Attack" type="number" label="Attack" handleChange={handleChange} value={formData.Attack} />
+              <FormItem name="Defense" type="number" label="Defense" handleChange={handleChange} value={formData.Defense} />
+            </div>
+            <div className="col-lg-6">
+              <FormItem name="Speed" type="number" label="Speed" handleChange={handleChange} value={formData.Speed} />
+              <FormItem name="SpAttack" type="number" label="Sp. Attack" handleChange={handleChange} value={formData.SpAttack} />
+              <FormItem name="SpDefense" type="number" label="Sp. Defense" handleChange={handleChange} value={formData.SpDefense} />
+            </div>
+          </div>
           <hr />
           <div className="text-center">
             <button className="btn btn-success" onClick={agregarHandle}>
